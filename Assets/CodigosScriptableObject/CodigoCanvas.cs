61814923f1bc9ed211146bc09344d60b25049729
Júlia <juliafcarvalho@gmail.com>
2021-10-17 18:16:33 -0300

    updates

--- Assets/CodigosScriptableObject/CodigoCanvas.cs
+++ Assets/CodigosScriptableObject/CodigoCanvas.cs
@@ -7,30 +7,36 @@ public class CodigoCanvas : Ouvinte
 {
     public ValoresJogador valores;
     public Ouvinte ouvinte;
-    public Slider barraDeVida;
-
+    public Text vidaJogador;
+    public Text fasesRealizadas;
+    public InimigoBase inimigo;
     private void Awake()
     {
         ouvinte = this.GetComponent<Ouvinte>();
-        barraDeVida = transform.GetChild(0).GetComponent<Slider>();
-        
+
+        vidaJogador = transform.GetChild(0).GetComponent<Text>();
+        fasesRealizadas = transform.GetChild(1).GetComponent<Text>();
+
+        inimigo = FindObjectOfType<InimigoBase>();
+
+
     }
     void Start()
     {
         valores = Jogador.jogador.valores;
         valores.RegisterListener(ouvinte);
-        barraDeVida.maxValue = valores.vidaMaxima;
-        barraDeVida.value = valores.vida;
+        vidaJogador.text = valores.vida.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fasesRealizadas.text = inimigo.etapasGanhas.ToString();
     }
 
     public override void JogadorLevouDano()
     {
-        barraDeVida.value = valores.vida;
+        vidaJogador.text = valores.vida.ToString();
+
     }
 }
